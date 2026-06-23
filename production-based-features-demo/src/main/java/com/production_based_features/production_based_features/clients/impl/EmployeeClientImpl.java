5be488fd0317d43eeda99b8f9801ef9f067a81a5
misty2810 <jangidmisty@gmail.com>
2026-06-23 09:31:01 +0530

    Updated production-based-features-demo

--- production-based-features-demo/src/main/java/com/production_based_features/production_based_features/clients/impl/EmployeeClientImpl.java
+++ production-based-features-demo/src/main/java/com/production_based_features/production_based_features/clients/impl/EmployeeClientImpl.java
@@ -6,6 +6,8 @@ import com.production_based_features.production_based_features.dto.EmployeeDTO;
 import com.production_based_features.production_based_features.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -19,43 +21,54 @@ import java.util.List;
 public class EmployeeClientImpl implements EmployeeClient {
 
     private final RestClient restClient;
+    Logger log= LoggerFactory.getLogger(EmployeeClientImpl.class);
 
     @Override
     public List<EmployeeDTO> getAllEmployees() {
+        log.trace("Trying to retrieve all employees in getAllEmployees");
         try {
-            return restClient.get()
+            log.info("Attempting to call the restClient Method in getAllEmployees");
+            List<EmployeeDTO> result= restClient.get()
                     .uri("emp")
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                        log.error(new String(res.getBody().readAllBytes()));
                         throw new ResourceNotFoundException("could not find employees");
                     })
                     .body(new ParameterizedTypeReference<List<EmployeeDTO>>() {
                     });
+            log.debug("Successfully retrieved the employees in getAllEmployees");
+            log.trace("Retrieved employees list in getAllEmployees : {}", result);
+            return result;
 
         } catch (Exception e) {
+            log.error("Exception occurred in getAllEmployees ",e);
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public EmployeeDTO getEmployeeById(Long employeeId) {
+        log.trace("Trying to get Employee By Id in getEmployeeById with id: {}", employeeId);
         try {
             return restClient.get()
                     .uri("emp/{employeeId}", employeeId)
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                        log.error(new String(res.getBody().readAllBytes()));
                         throw new ResourceNotFoundException("could not find employee");
                     })
                     .body(EmployeeDTO.class);
 
         } catch (Exception e) {
+            log.error("Exception occurred in getAllEmployees", e);
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public EmployeeDTO createNewEmployee(EmployeeDTO employeeDTO) {
-
+        log.trace("Trying to create Employee with information {}", employeeDTO);
         try {
             EmployeeDTO savedEmployee =
                     restClient.post()
@@ -63,13 +76,16 @@ public class EmployeeClientImpl implements EmployeeClient {
                             .body(employeeDTO)
                             .retrieve()
                             .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                                log.debug("4xxClient error occurred during createNewEmployee");
+                                log.error(new String(res.getBody().readAllBytes()));
                                 throw new ResourceNotFoundException("could not create employee");
                             })
                             .body(EmployeeDTO.class);
-
+            log.trace("Successfully created a new employee : {}", savedEmployee);
             return savedEmployee;
 
         } catch (Exception e) {
+            log.error("Exception occurred in getAllEmployees", e);
             throw new RuntimeException(e);
         }
     }
